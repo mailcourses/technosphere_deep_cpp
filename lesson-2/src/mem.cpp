c55712faa19b6031aa701af725498882cc0e089f
Anton Kukhtichev <a.kukhtichev@mail.ru>
2021-03-22 13:40:15 +0300

    Обновил лекцию и немножко домашнее задание.

--- lesson-2/src/mem.cpp
+++ lesson-2/src/mem.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+int64_t global = 1;
+
 int main()
 {
     int64_t i = 1;
@@ -9,11 +11,12 @@ int main()
         int32_t, int64_t
         uint32_t, uint64_t
     */
-    std::cout << std::hex << static_cast<int64_t *>(&i) << std::endl;
-    std::cout << std::hex << static_cast<int64_t *>(pi) << std::endl;
-    std::cout << std::hex << (int64_t *)main << std::endl;
+    std::cout << "Stack: " << std::hex << static_cast<int64_t *>(&i) << std::endl;
+    std::cout << "Heap: " << std::hex << static_cast<int64_t *>(pi) << std::endl;
+    std::cout << "Global: " << std::hex << static_cast<int64_t *>(&global) << std::endl;
+    std::cout << "Main function 1: " << std::hex << (int64_t *)main << std::endl;
     // ... или
-    std::cout << std::hex << reinterpret_cast<void*>(main) << std::endl;
+    std::cout << "Main function 2: " << std::hex << reinterpret_cast<void*>(main) << std::endl;
 
     std::cin.get();
 
